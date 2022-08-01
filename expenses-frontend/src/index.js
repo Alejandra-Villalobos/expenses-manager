cbd4a988c579b8e2920b5f86d8badacc2f0142ce
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-08-01 16:19:58 -0600

    Deleted: Layout, warnings fixed

--- expenses-frontend/src/index.js
+++ expenses-frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
-import App from './App';
-import Layout from './pages/Layout';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
 
@@ -11,10 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Auth/>}/>
-        <Route path='/home' element={<Home/>}/>
-      </Route>
+      <Route index element={<Auth/>}/>
+      <Route path='/home' element={<Home/>}/>
     </Routes>
   </BrowserRouter>
 );

cfbfb5d80e8a3f87e6b359ad5a10f344e96bbaf1
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-08-04 19:48:14 -0600

    Added: Banks interface improved

--- expenses-frontend/src/pages/Banks.js
+++ expenses-frontend/src/pages/Banks.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import SideMenu from '../components/SideMenu.js'
 import NavBar from '../components/NavBar.js'
+import { MdAddCircle } from 'react-icons/md'
 
 const Banks = () => {
   const [showMenu, setShowMenu] = useState(true)
@@ -9,12 +10,12 @@ const Banks = () => {
     <NavBar setState={setShowMenu} state={showMenu}/>
     <div className='flex flex-row'>
     {showMenu && <SideMenu/>}
-      <div>
-        <section>
+      <div className='w-full'>
+        <section className='flex flex-row justify-center items-center gap-x-5 mt-8'>
           <h1>Bank Accounts</h1>
-          <button>Add bank</button>
+          <button><MdAddCircle size={25}/></button>
         </section>
-        <section>
+        <section className='flex justify-center mt-8'>
           <h2>Banks</h2>
         </section>
       </div>

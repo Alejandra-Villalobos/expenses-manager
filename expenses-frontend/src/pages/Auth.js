6fbb065727a33fd71e239fb0f6fb805924ba26d8
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-08-05 19:52:43 -0600

    Added: Auth style

--- expenses-frontend/src/pages/Auth.js
+++ expenses-frontend/src/pages/Auth.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState } from 'react'
 import { Outlet, Link } from "react-router-dom"
 import loginImg from '../assets/login.png'
-import registerImg from '../assets/register.png'
 
 
 
@@ -50,31 +49,31 @@ const Login = () => {
     }
 
     return (
-        <div className='bg-blue-900 h-screen'>
+        <div className='bg-gradient-to-br from-sky-400 to-indigo-900 h-screen'>
             <div className='h-screen'>
-                <h1 className='text-center font-bold text-2xl pt-8'>Welcome to Expenses Manager!</h1>
+                <h1 className='text-center text-white pt-12 font-fira font-bold text-4xl'>Welcome to Expenses Manager!</h1>
                 {showLoginForm &&
                 <div className='flex flex-row justify-around items-center h-5/6'>
-                    <img src={loginImg} alt='login' className='w-80 h-80'/>
-                    <form onSubmit={handleSubmit} className="bg-blue-500 rounded-lg border-blue-400 border-2 p-5">
+                    <img src={loginImg} alt='login' className='w-80 h-80 drop-shadow-md'/>
+                    <form onSubmit={handleSubmit} className="bg-white rounded-lg border-gray-300 shadow-md border-1 p-5">
                         <div class="grid gap-6 mb-6 md:grid-cols-1">
                             <div>
                                 <label for="userName" class="block mb-2 text-sm font-medium text-gray-900 ">User</label>
-                                <input type="text" id="userName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
+                                <input type="text" id="userName" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
                                 value={formValues.userName || ""}
                                 onChange={handeFormChange}
                                 name="userName"/>
                             </div>
-                            <div class="mb-6">
+                            <div>
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
+                                <input type="password" id="password" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
                                 value={formValues.userPass || ""}
                                 onChange={handeFormChange}
                                 name="userPass"/>
                             </div> 
                             <div className='grid gap-6 mb-6 md:grid-cols-2'>
-                                <Link to={'/home'}><input type="submit" value="Iniciar Sesión" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
-                                <button onClick={onShowRegForm} className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">No tienes cuenta?</button>
+                                <Link to={'/home'}><input type="submit" value="Iniciar Sesión" class="text-white shadow-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
+                                <button onClick={onShowRegForm} className="text-white shadow-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">No tienes cuenta?</button>
                             </div>
                         </div>
                     </form>
@@ -82,32 +81,32 @@ const Login = () => {
                 {showRegisterForm &&
                 <div className='flex flex-row justify-around items-center h-5/6'>
                 <img src={loginImg} alt='register' className='w-80 h-80'/>
-                <form onSubmit={handleSubmit} className="rounded-lg border-cyan-400 border-2 p-5">
+                <form onSubmit={handleSubmit} className="bg-white rounded-lg border-gray-300 shadow-md border-1 p-5">
                     <div class="grid gap-6 mb-6 md:grid-cols-1">
                         <div>
                             <label for="userName" class="block mb-2 text-sm font-medium text-gray-900 ">User</label>
-                            <input type="text" id="userName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
+                            <input type="text" id="userName" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
                             name='userName'
                             value={formRegValues.userName || ""}
                             onChange={handeFormRegChange}/>
                         </div>
-                        <div class="mb-6">
+                        <div>
                             <label for="userMail" class="block mb-2 text-sm font-medium text-gray-900">Email address</label>
-                            <input type="email" id="userMail" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="john.doe@company.com" required
+                            <input type="email" id="userMail" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required
                             name='userMail'
                             value={formRegValues.userMail || ""}
                             onChange={handeFormRegChange}/>
                         </div> 
-                        <div class="mb-6">
+                        <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
+                            <input type="password" id="password" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
                             name='userPass'
                             value={formRegValues.userPass || ""}
                             onChange={handeFormRegChange}/>
                         </div> 
                         <div className='grid gap-6 mb-6 md:grid-cols-2'>
-                            <Link to={'/home'}><input type="submit" value="Registrarse" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
-                            <button onClick={onShowLoginForm} className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Ya tienes cuenta?</button>
+                            <Link to={'/home'}><input type="submit" value="Registrarse" class="text-white shadow-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
+                            <button onClick={onShowLoginForm} className="text-white shadow-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Ya tienes cuenta?</button>
                         </div>
                     </div>
                 </form>
--- expenses-frontend/tailwind.config.js
+++ expenses-frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fira': ['Fira Sans Condensed', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }

fc71abe7d8544135dc5419b28c9aae8cc6b9fac4
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-08-07 15:31:26 -0600

    Added: Handle bank form

--- expenses-frontend/src/components/BankForm.js
+++ expenses-frontend/src/components/BankForm.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Popup(props) {
+  const [newBank, setNewBank] = useState([]);
+
+  const handeFormChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setNewBank((values)=>{
+        return{
+            ...values,
+            [name]: value
+        };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bank data: ${newBank.bankName} ${newBank.userName} ${newBank.amount}`)
+    setNewBank([])
+    props.setTrigger(false)
+  };
+
   return (props.trigger) ? (
     <div className='fixed top-0 left-0 w-full h-screen flex justify-center items-center bg-black bg-opacity-40'>
-        <div className='relative p-8 bg-teal-500 rounded-md'>
-            <form className='flex flex-col'>
+        <div className='relative p-8 bg-white rounded-md'>
+            <form className='flex flex-col' onSubmit={handleSubmit}>
+              <h1 className='text-center font-bold mb-6 text-xl'>Add Bank</h1>
               <label>Bank Name</label>
-              <input className='rounded mb-6' type="text"  name='bankName'/>
+              <input className=' mb-6 shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                type="text"
+                value={newBank.bankName || ""}
+                onChange={handeFormChange} 
+                name='bankName'
+                placeholder="Banco Central" required/>
               <label>User Name</label>
-              <input className='rounded mb-6' type="text"  name='userName'/>
+              <input className=' mb-6 shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                type="text"
+                value={newBank.userName || ""}
+                onChange={handeFormChange}
+                name='userName'
+                placeholder="John Doe" required/>
               <label>Amount</label>
-              <input className='rounded mb-6' type="number" name='amount'/>
+              <input className=' mb-6 shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                type="number"
+                value={newBank.amount || ""}
+                onChange={handeFormChange}
+                name='amount'
+                placeholder="3000.00" required/>
               <div className='flex flex-row justify-between'>
                 <button className='flex items-center shadow-md bg-red-500 rounded-lg px-5 py-2' onClick={() => props.setTrigger(false)}>Cancel</button>
                 <input className='flex items-center shadow-md bg-green-500 rounded-lg px-5 py-2' type='submit' value='Add'/>
--- expenses-frontend/src/pages/Auth.js
+++ expenses-frontend/src/pages/Auth.js
@@ -3,8 +3,6 @@ import { useState } from 'react'
 import { Outlet, Link } from "react-router-dom"
 import loginImg from '../assets/login.png'
 
-
-
 const Login = () => {
     const [formValues, setFormValues] = useState([]);
     const [formRegValues, setFormRegValues] = useState([]);
@@ -56,23 +54,23 @@ const Login = () => {
                 <div className='flex flex-row justify-around items-center h-5/6'>
                     <img src={loginImg} alt='login' className='w-80 h-80 drop-shadow-md'/>
                     <form onSubmit={handleSubmit} className="bg-white rounded-lg border-gray-300 shadow-md border-1 p-5">
-                        <div class="grid gap-6 mb-6 md:grid-cols-1">
+                        <div className="grid gap-6 mb-6 md:grid-cols-1">
                             <div>
-                                <label for="userName" class="block mb-2 text-sm font-medium text-gray-900 ">User</label>
-                                <input type="text" id="userName" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
+                                <label for="userName" className="block mb-2 text-sm font-medium text-gray-900 ">User</label>
+                                <input type="text" id="userName" className="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
                                 value={formValues.userName || ""}
                                 onChange={handeFormChange}
                                 name="userName"/>
                             </div>
                             <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                <input type="password" id="password" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
+                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                                <input type="password" id="password" className="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
                                 value={formValues.userPass || ""}
                                 onChange={handeFormChange}
                                 name="userPass"/>
                             </div> 
                             <div className='grid gap-6 mb-6 md:grid-cols-2'>
-                                <Link to={'/home'}><input type="submit" value="Iniciar Sesión" class="text-white shadow-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
+                                <Link to={'/home'}><input type="submit" value="Iniciar Sesión" className="text-white shadow-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
                                 <button onClick={onShowRegForm} className="text-white shadow-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">No tienes cuenta?</button>
                             </div>
                         </div>
@@ -82,30 +80,30 @@ const Login = () => {
                 <div className='flex flex-row justify-around items-center h-5/6'>
                 <img src={loginImg} alt='register' className='w-80 h-80'/>
                 <form onSubmit={handleSubmit} className="bg-white rounded-lg border-gray-300 shadow-md border-1 p-5">
-                    <div class="grid gap-6 mb-6 md:grid-cols-1">
+                    <div className="grid gap-6 mb-6 md:grid-cols-1">
                         <div>
-                            <label for="userName" class="block mb-2 text-sm font-medium text-gray-900 ">User</label>
-                            <input type="text" id="userName" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
+                            <label for="userName" className="block mb-2 text-sm font-medium text-gray-900 ">User</label>
+                            <input type="text" id="userName" className="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required 
                             name='userName'
                             value={formRegValues.userName || ""}
                             onChange={handeFormRegChange}/>
                         </div>
                         <div>
-                            <label for="userMail" class="block mb-2 text-sm font-medium text-gray-900">Email address</label>
-                            <input type="email" id="userMail" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required
+                            <label for="userMail" className="block mb-2 text-sm font-medium text-gray-900">Email address</label>
+                            <input type="email" id="userMail" className="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required
                             name='userMail'
                             value={formRegValues.userMail || ""}
                             onChange={handeFormRegChange}/>
                         </div> 
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                            <input type="password" id="password" class="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
+                            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                            <input type="password" id="password" className="bg-white shadow-inner border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required 
                             name='userPass'
                             value={formRegValues.userPass || ""}
                             onChange={handeFormRegChange}/>
                         </div> 
                         <div className='grid gap-6 mb-6 md:grid-cols-2'>
-                            <Link to={'/home'}><input type="submit" value="Registrarse" class="text-white shadow-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
+                            <Link to={'/home'}><input type="submit" value="Registrarse" className="text-white shadow-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"/></Link>
                             <button onClick={onShowLoginForm} className="text-white shadow-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Ya tienes cuenta?</button>
                         </div>
                     </div>
